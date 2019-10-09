9d5dfb5bb41d4f658a68fc665cfd065a58c8b6ab
BASILE MINVIELLE <bminvielle@lucca.fr>
2019-10-09 14:25:28 +0000

    Categories modifications

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,14 +1,13 @@
 categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
 
-
 puts "What activity do you want to add ?"
-  categories << gets.chomp
+categories << gets.chomp
 
-  categories = categories.sort
+categories = categories.sort
 puts "[CATEGORIES OF ACTIVITIES]"
 
-  categories.each do |activity|
-    puts "- #{activity}"
-  end
+categories.each do |activity|
+  puts "- #{activity}"
+end
 
-  puts "Total: #{categories.size}"
+puts "Total: #{categories.size}"

3ae976b00e33a7c5744084a11981f18b3f5412ff
BASILE MINVIELLE <bminvielle@lucca.fr>
2019-10-09 14:16:13 +0000

    listing ex

--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,51 @@
-activities = [
-  # TODO: gather at least 3 activities
-]
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
 
-# TODO: your code goes here
+activities = [
+  {
+    "name" => "Escape Game",
+    "category" => categories[0].upcase,
+    "city" => "Nantes",
+    "duration" => 60,
+    "participants" => 4,
+    "price" => 120
+    },
+    {
+    "name" => "Wine Tasting",
+    "category" => categories[5].upcase,
+    "city" => "Paris",
+    "duration" => 90,
+    "participants" => 15,
+    "price" => 300
+    },
+    {
+    "name" => "Rugby",
+    "category" => categories[1].upcase,
+    "city" => "Marseille",
+    "duration" => 80,
+    "participants" => 46,
+    "price" => 200
+    }
+  ]
+
+
+puts "#{activities[0]["name"]} [#{activities[0]["category"]}]"
+puts "Located in #{activities[0]["city"]}"
+puts "Duration of #{activities[0]["duration"]} minutes"
+puts "For #{activities[0]["participants"]} participants"
+puts "Charged #{activities[0]["price"]}€"
+
+puts "
+---
+
+"
+
+puts "#{activities[1]["name"]} [#{activities[1]["category"]}]"
+puts "Located in #{activities[1]["city"]}"
+puts "Duration of #{activities[1]["duration"]} minutes"
+puts "For #{activities[1]["participants"]} participants"
+puts "Charged #{activities[1]["price"]}€"
+
+puts "
+---
+
+"

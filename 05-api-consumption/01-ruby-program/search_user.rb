e651b5998a77ca6271206849bbc5d4df8e19c877
BASILE MINVIELLE <bminvielle@lucca.fr>
2019-10-11 14:15:18 +0000

    Commit ruby pgm with API

--- 05-api-consumption/01-ruby-program/search_user.rb
+++ 05-api-consumption/01-ruby-program/search_user.rb
@@ -1,9 +1,23 @@
 require "json"
 require "rest-client"
 
-puts "What is the name of the user you're looking for? (e.g. Bob Dylan)"
+puts "What is the name of the user you're looking for? (Pleeeeaaase enter the first name first then the last name)"
 name = gets.chomp # asks user to type something in the Terminal
+
+name2 = name.split
+
+firstname = name2[0]
+lastname = name2[1]
 
 puts "Ok got it! Let's search for that person..."
 
-# TODO: your code goes here
+token = "18afcfb8-697b-4742-b726-418269485cee"
+url = "https://wagon.ilucca-demo.net/api/v3/users?firstName=#{firstname}&lastName=#{lastname}&fields=firstName,lastName,mail,jobTitle,department.name"
+headers = { "Authorization" => "Lucca application=#{token}" }
+response = RestClient.get(url, headers)
+payload = JSON.parse(response.body)
+
+
+puts "#{payload["data"]["items"][0]["firstName"]} #{payload["data"]["items"][0]["lastName"]} (#{payload["data"]["items"][0]["mail"]})"
+puts "#{payload["data"]["items"][0]["jobTitle"]}"
+puts "From #{payload["data"]["items"][0]["department"]["name"]}"
--- 05-api-consumption/01-ruby-program/users_by_role.rb
+++ 05-api-consumption/01-ruby-program/users_by_role.rb
@@ -2,8 +2,33 @@ require "json"
 require "rest-client"
 
 puts "What is the role of the users you're looking for? (e.g. Figgo - Manager)"
-role = gets.chomp # asks user to type something in the Terminal
+role_name = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it! Let's search for the users..."
 
-# TODO: your code goes here
+
+token = "18afcfb8-697b-4742-b726-418269485cee"
+headers = { "Authorization" => "Lucca application=#{token}" }
+url = "https://wagon.ilucca-demo.net/api/v3/roles?name=#{role_name}"
+response = RestClient.get(url, headers)
+payload = JSON.parse(response.body)
+
+role_id = payload["data"]["items"][0]["id"]
+
+url = "https://wagon.ilucca-demo.net/api/v3/users?fields=name,jobTitle&rolePrincipalid=#{role_id}"
+response = RestClient.get(url, headers)
+payload = JSON.parse(response.body)
+
+total_count = payload["data"]["items"].count
+
+
+puts "Total: #{total_count} people found"
+puts ""
+
+payload["data"]["items"].each do |list|
+
+puts "#{list["name"]}"
+puts "#{list["jobTitle"]}"
+puts "---"
+
+end

f11fbea936296c2eb80b21fd1738b35bda77e48b
BASILE MINVIELLE <bminvielle@lucca.fr>
2019-10-11 16:10:46 +0000

    Modif app

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -4,15 +4,13 @@ require "sinatra/reloader" if development?
 
 enable :static
 
+CATEGORIES = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
 
-
-get "/" do
-@categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
-
-@activities = [
+ACTIVITIES = [
   {
+    "id" => 0,
     "name" => "Escape Game",
-    "category" => @categories[0].upcase,
+    "category" => CATEGORIES[0].upcase,
     "city" => "Nantes",
     "duration" => 60,
     "participants" => 4,
@@ -20,8 +18,9 @@ get "/" do
     "image" => "https://cdn.futura-sciences.com/buildsv6/images/wide1920/9/d/e/9de5f7181a_126842_lobby-1-c-escape-hunt-nantes.jpg"
     },
     {
+    "id" => 1,
     "name" => "Wine Tasting",
-    "category" => @categories[5].upcase,
+    "category" => CATEGORIES[5].upcase,
     "city" => "Paris",
     "duration" => 90,
     "participants" => 15,
@@ -29,8 +28,9 @@ get "/" do
     "image" => "https://img.mshanken.com/d/wso/Articles/2019/IQ_HowToTasting_1600.jpg"
     },
     {
+    "id" => 2,
     "name" => "Rugby",
-    "category" => @categories[1].upcase,
+    "category" => CATEGORIES[1].upcase,
     "city" => "Marseille",
     "duration" => 80,
     "participants" => 46,
@@ -38,8 +38,9 @@ get "/" do
     "image" => "http://www.leparisien.fr/resizer/xHNPY1i61PTOV_5soJFe6NyRy7E=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/GD2F7SAOD3Z3QAVR2GQSZKSLZI.jpg"
     },
     {
+    "id" => 3,
     "name" => "Karting",
-    "category" => @categories[1].upcase,
+    "category" => CATEGORIES[1].upcase,
     "city" => "Nice",
     "duration" => 120,
     "participants" => 10,
@@ -47,8 +48,9 @@ get "/" do
     "image" => "https://images.caradisiac.com/images/4/5/9/9/174599/S0-test-kartkraft-le-simulateur-de-karting-pour-fanatiques-582283.jpg"
     },
     {
+    "id" => 4,
     "name" => "Karaoke",
-    "category" => @categories[4].upcase,
+    "category" => CATEGORIES[4].upcase,
     "city" => "Tokyo",
     "duration" => 120,
     "participants" => 100,
@@ -56,8 +58,9 @@ get "/" do
     "image" => "http://goolfy-montpellier.com/wp-content/uploads/2018/06/karaoke.jpg"
     },
     {
+    "id" => 5,
     "name" => "Pole Dance",
-    "category" => @categories[4].upcase,
+    "category" => CATEGORIES[4].upcase,
     "city" => "Rennes",
     "duration" => 60,
     "participants" => 5,
@@ -65,8 +68,9 @@ get "/" do
     "image" => "https://img.grouponcdn.com/deal/e27py5JFcfDuLHZi137xD3qNZCk/e2-700x420/v1/c700x420.jpg"
     },
     {
+    "id" => 6,
     "name" => "Apéro",
-    "category" => @categories[0].upcase,
+    "category" => CATEGORIES[0].upcase,
     "city" => "Marseille",
     "duration" => 300,
     "participants" => 1000,
@@ -75,6 +79,16 @@ get "/" do
     }
   ]
 
+get "/" do
+
+@activities = ACTIVITIES
+
   erb :index
 
+end
+
+get "/activities/:id" do
+  @activity = ACTIVITIES.find { |activity| activity["id"] == params["id"].to_i }
+
+  erb :activity
 end

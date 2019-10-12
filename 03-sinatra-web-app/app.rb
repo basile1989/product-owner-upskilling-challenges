dfffb2b9a87af5cf5e5d67864366b5d233775243
BASILE MINVIELLE <bminvielle@lucca.fr>
2019-10-12 11:13:37 +0000

    Add activity page

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -15,6 +15,8 @@ ACTIVITIES = [
     "duration" => 60,
     "participants" => 4,
     "price" => 120,
+    "icon" => "fas fa-hiking",
+    "description" => "Escape game, is a subgenre of point-and-click adventure game which requires a player to escape from imprisonment by exploiting their surroundings.",
     "image" => "https://cdn.futura-sciences.com/buildsv6/images/wide1920/9/d/e/9de5f7181a_126842_lobby-1-c-escape-hunt-nantes.jpg"
     },
     {
@@ -25,6 +27,8 @@ ACTIVITIES = [
     "duration" => 90,
     "participants" => 15,
     "price" => 300,
+    "icon" => "fas fa-utensils",
+    "description" => "Wine tasting is the sensory examination and evaluation of wine. While the practice of wine tasting is as ancient as its production, a more formalized methodology has slowly become established from the 14th century onwards.",
     "image" => "https://img.mshanken.com/d/wso/Articles/2019/IQ_HowToTasting_1600.jpg"
     },
     {
@@ -35,6 +39,8 @@ ACTIVITIES = [
     "duration" => 80,
     "participants" => 46,
     "price" => 200,
+    "icon" => "far fa-futbol",
+    "description" => "Rugby, is a contact team sport which originated in England in the first half of the 19th century. One of the two codes of rugby football, it is based on running with the ball in hand.",
     "image" => "http://www.leparisien.fr/resizer/xHNPY1i61PTOV_5soJFe6NyRy7E=/932x582/arc-anglerfish-eu-central-1-prod-leparisien.s3.amazonaws.com/public/GD2F7SAOD3Z3QAVR2GQSZKSLZI.jpg"
     },
     {
@@ -45,6 +51,8 @@ ACTIVITIES = [
     "duration" => 120,
     "participants" => 10,
     "price" => 300,
+    "icon" => "far fa-futbol",
+    "description" => "Karting is a variant of motorsport road racing with open-wheel, four-wheeled vehicles known as go-karts or shifter karts. They are usually raced on scaled-down circuits.",
     "image" => "https://images.caradisiac.com/images/4/5/9/9/174599/S0-test-kartkraft-le-simulateur-de-karting-pour-fanatiques-582283.jpg"
     },
     {
@@ -55,6 +63,8 @@ ACTIVITIES = [
     "duration" => 120,
     "participants" => 100,
     "price" => 600,
+    "icon" => "fas fa-book",
+    "description" => "Karaoke is a way of dining interactive entertainment or video game developed in Japan in which an amateur singer sings along with recorded music (a music video) using a microphone.",
     "image" => "http://goolfy-montpellier.com/wp-content/uploads/2018/06/karaoke.jpg"
     },
     {
@@ -65,6 +75,8 @@ ACTIVITIES = [
     "duration" => 60,
     "participants" => 5,
     "price" => 300,
+    "icon" => "fas fa-book",
+    "description" => "Pole dance combines dance and acrobatics centered on a vertical pole. This performance art form takes place not only in gentleman's clubs as erotic dance, but has also recently gained popularity as a mainstream form of fitness, practiced by many enthusiasts in gyms and in dedicated dance studios.",
     "image" => "https://img.grouponcdn.com/deal/e27py5JFcfDuLHZi137xD3qNZCk/e2-700x420/v1/c700x420.jpg"
     },
     {
@@ -75,6 +87,8 @@ ACTIVITIES = [
     "duration" => 300,
     "participants" => 1000,
     "price" => 10000,
+    "icon" => "fas fa-hiking",
+    "description" => "Best way to end the week, or the day, apéro comes from the south of France and is now a global story.",
     "image" => "https://www.cherbougetoi.com/wp-content/uploads/2017/04/beerz-apero.jpg"
     }
   ]

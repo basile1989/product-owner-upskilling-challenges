ab2847ba198eef343702e2304f9ff3383a0c0988
BASILE MINVIELLE <bminvielle@lucca.fr>
2019-10-16 15:58:10 +0000

    Web with API calls

--- 05-api-consumption/02-sinatra-web-app/app.rb
+++ 05-api-consumption/02-sinatra-web-app/app.rb
@@ -7,8 +7,92 @@ require "sinatra/reloader" if development?
 
 enable :static
 
+
+
 get "/" do
-  @activities = # TODO: retrieve all the activities from the API
+  url = "https://team-building-api.cleverapps.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  @activities = payload["activities"]
+
+  url = "https://team-building-api.cleverapps.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  list = payload["activities"]
+  categories = []
+  cities = []
+  list.each do |activity|
+    cities = cities + [activity["city"]]
+    categories = categories + [activity["category"]]
+  end
+
+  @city_list = cities.uniq
+  @category_list = categories.uniq
 
   erb :index
+end
+
+get "/activities/:id" do
+
+  url = "https://team-building-api.cleverapps.io/v2/activities/#{params["id"]}"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  @activity = payload["activity"]
+
+  erb :activity
+end
+
+get "/activities" do
+
+
+  url = "https://team-building-api.cleverapps.io/v2/activities?city=#{params["city"]}"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  @activities = payload["activities"]
+
+  url = "https://team-building-api.cleverapps.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  list = payload["activities"]
+  categories = []
+  cities = []
+  list.each do |activity|
+    cities = cities + [activity["city"]]
+    categories = categories + [activity["category"]]
+  end
+
+  @city_list = cities.uniq
+  @category_list = categories.uniq
+
+
+
+  erb :activity_by_city
+end
+
+
+get "/activities" do
+
+
+  url = "https://team-building-api.cleverapps.io/v2/activities?category=#{params["category"]}"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  @activities = payload["activities"]
+
+  url = "https://team-building-api.cleverapps.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  list = payload["activities"]
+  categories = []
+  cities = []
+  list.each do |activity|
+    cities = cities + [activity["city"]]
+    categories = categories + [activity["category"]]
+  end
+
+  @city_list = cities.uniq
+  @category_list = categories.uniq
+
+
+
+  erb :activity_by_category
 end

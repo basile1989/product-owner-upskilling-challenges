e7d383d171eb4f2b15e2bae0dfbcd046b0e35c8d
BASILE MINVIELLE <bminvielle@lucca.fr>
2019-10-17 12:15:21 +0000

    create API and modif web sinatra

--- 06-building-api/app.rb
+++ 06-building-api/app.rb
@@ -16,9 +16,64 @@ get "/" do
   json "name" => "Team Building API", "status" => "Running"
 end
 
+
+
 namespace "/v1" do
   # TODO: your code goes here
+  get "/activities" do
+    activities = DB.execute("SELECT * FROM activities;")
+    json "activities" => activities
+  end
+  get "/activities/:id" do
+    activity = DB.execute("SELECT * FROM activities where id = #{params["id"]};")
+    json "activity" => activity
+
+  end
 end
+
+
+
+namespace "/v2" do
+  # TODO: your code goes here
+
+  get "/activities" do
+
+
+    if params["city"] != nil
+      city_query = "upper(city) = upper(\"#{params["city"]}\")"
+      filter = "where"
+    end
+
+    if params["category"] != nil
+      category_query = "upper(category) = upper(\"#{params["category"]}\")"
+      filter = "where"
+    end
+
+    if params["search"] != nil
+      name_query = "name LIKE \"%#{params["search"]}%\" "
+      filter = "where"
+    end
+
+
+    if params["search"] == nil && params["category"] == nil && params["city"] == nil
+      filter = ""
+    end
+
+    query = [city_query, category_query, name_query].compact.join(" and ")
+    activities = DB.execute("SELECT * FROM activities #{filter} #{query};")
+    json "activities" => activities
+
+
+  end
+  get "/activities/:id" do
+    activity = DB.execute("SELECT * FROM activities where id = #{params["id"]};")
+    json "activity" => activity
+
+  end
+end
+
+
+
 
 namespace "/doc" do
   get { erb :"doc/index" }

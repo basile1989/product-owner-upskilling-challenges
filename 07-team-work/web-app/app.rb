b118d7504b2ee71328799fd44ba64044420957a4
BASILE MINVIELLE <bminvielle@lucca.fr>
2019-10-18 14:35:12 +0000

    Push de tout le HTML CSS RB

--- 07-team-work/web-app/app.rb
+++ 07-team-work/web-app/app.rb
@@ -7,30 +7,166 @@ require "sinatra/reloader" if development?
 
 enable :static
 
-get "/" do
-  url        = "http://localhost:4567/v2/activities"
-  api_params = {}
 
-  # filtering
-  if params["location"] && !params["location"].empty?
-    api_params["city"] = params["location"]
+
+get "/" do
+  url = "693c90d8.ngrok.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  @activities = payload["activities"]
+
+  url = "693c90d8.ngrok.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  list = payload["activities"]
+  categories = []
+  cities = []
+  list.each do |activity|
+    cities = cities + [activity["city"]]
+    categories = categories + [activity["category"]]
   end
 
-  response   = RestClient.get(url, params: api_params)
-  payload    = JSON.parse(response.body)
-
-  @activities = payload["activities"]
+  @city_list = cities.uniq
+  @category_list = categories.uniq
 
   erb :index
 end
 
-get "/activities/:id" do
-  id       = params["id"]
-  url      = "http://localhost:4567/v2/activities/#{id}"
-  response = RestClient.get(url)
-  payload  = JSON.parse(response.body)
 
+
+
+
+get "/activities/:id" do
+
+  url = "693c90d8.ngrok.io/v2/activities/#{params["id"]}"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
   @activity = payload["activity"]
 
-  erb :show
+  url = "693c90d8.ngrok.io/v2/teams"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+
+  @teams = payload["teams"]
+
+  url = "693c90d8.ngrok.io/v2/favorites?activity_id=#{params["id"]}"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+
+  if payload != nil
+  @favorites = payload["favorites"]
+  end
+
+  erb :activity
+end
+
+
+
+
+
+get "/activities" do
+
+
+  url = "693c90d8.ngrok.io/v2/activities?city=#{params["city"]}&category=#{params["category"]}"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  @activities = payload["activities"]
+
+  url = "693c90d8.ngrok.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  list = payload["activities"]
+  categories = []
+  cities = []
+  list.each do |activity|
+    cities = cities + [activity["city"]]
+    categories = categories + [activity["category"]]
+  end
+
+  @city_list = cities.uniq
+  @category_list = categories.uniq
+
+
+
+  erb :activity_filtered
+end
+
+get "/activities" do
+
+
+  url = "693c90d8.ngrok.io/v2/activities?search=#{params["name"]}"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  @activities = payload["activities"]
+
+  url = "693c90d8.ngrok.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  list = payload["activities"]
+  categories = []
+  cities = []
+  list.each do |activity|
+    cities = cities + [activity["city"]]
+    categories = categories + [activity["category"]]
+  end
+
+  @city_list = cities.uniq
+  @category_list = categories.uniq
+
+
+
+  erb :activity_filtered
+end
+
+get "/wishlists" do
+  url = "693c90d8.ngrok.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  @activities = payload["activities"]
+
+  url = "693c90d8.ngrok.io/v2/activities"
+  response = RestClient.get(url)
+  payload = JSON.parse(response.body)
+  list = payload["activities"]
+  categories = []
+  cities = []
+  list.each do |activity|
+    cities = cities + [activity["city"]]
+    categories = categories + [activity["category"]]
+  end
+
+  @city_list = cities.uniq
+  @category_list = categories.uniq
+
+  erb :wishlists
+end
+
+
+post "/activity/newlike" do
+
+  api_params = {
+    "team_id" => params["team_id"] ,
+    "activity_id" => params["activity_id"]
+  }
+
+  url = "http://693c90d8.ngrok.io/v2/favorites"
+
+  response = RestClient.post(url, api_params)
+  wishlist = JSON.parse(response.body)
+
+  redirect to("http://localhost:4567/activities/#{params["activity_id"]}")
+
+end
+
+delete "/remove_from_wishlist" do
+
+  api_params = {
+    "team_id" => params["team_id"]
+  }
+
+  url = "http://693c90d8.ngrok.io/v2/favorites"
+
+  response = RestClient.post(url, api_params)
+  wishlist = JSON.parse(response.body)
+
 end
